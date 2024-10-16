d655d41463651816cf0fec7be7adb6ac6e8a2160
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-16 18:03:12 +0200

    working on snack 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,12 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+// Uso find per trovare la classe di uno studente specifico
+const student1Class = students.find((student) => {
+  if (student.name = 'Marco Lanci'){
+    return students.class
+  }
+  return none
+})
+console.log(student1Class);
