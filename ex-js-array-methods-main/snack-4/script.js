73f81b33ffcb4ecff78e413a4c5a58bce972e0bf
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-16 16:43:15 +0200

    snack 4 map

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -10,7 +10,17 @@ console.log(posts);
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
 
-const authors = posts.filter((posts) => {
-  return posts.author
+const authors = posts.map((author) => {
+  return author;
 });
 console.log(authors);
+
+
+
+
+
+
+/* const authors = posts.filter((posts) => {
+  return posts.author
+});
+console.log(authors); */
